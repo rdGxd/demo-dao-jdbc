9fe1ee494321704b2f2523d3ac3b084f1d798647
Rodrigo Silva <rdg.hora1@gmail.com>
2024-04-08 11:35:48 -0300

    Department entity class

---
+++ src/model/entities/Seller.java
@@ -0,0 +1,59 @@
+package model.entities;
+
+import java.util.Date;
+
+public class Seller {
+    private Integer id;
+    private String name;
+    private String email;
+    private Date birthDate;
+    private Double baseSalary;
+
+    public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.birthDate = birthDate;
+        this.baseSalary = baseSalary;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public void setBaseSalary(Double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public Double getBaseSalary() {
+        return baseSalary;
+    }
+}
