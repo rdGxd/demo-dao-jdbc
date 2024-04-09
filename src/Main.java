53bcc44d8664cead4004aa36192f1fab63a37766
Rodrigo Silva <rdg.hora1@gmail.com>
2024-04-09 11:12:47 -0300

    SellerDaoJDBC and DaoFactory

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -7,6 +9,9 @@ public class Main {
     public static void main(String[] args) {
         Department obj = new Department(1, "Books");
         Seller seller = new Seller(1, "Rodrigo", "rodrigo@gmail.com", new Date(), 3000.00, obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
         System.out.println(seller);
     }
 }
